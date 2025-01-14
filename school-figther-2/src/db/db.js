b2b01a42acade4e9c1826fc88798793397cfd2a5
cristofercun97 <cris99991@hotmail.com>
2025-01-14 14:05:46 +0100

    Completado el diseño de la vista Personaje

---
+++ school-figther-2/src/db/db.js
@@ -0,0 +1,11 @@
+export const personajes =[
+    {
+        nombre: 'Soldado Romano',
+        imagen: 'https://media2.giphy.com/media/1xmSRp4Ufqx9B98X4f/200w.gif?cid=82a1493beff0nxbmipxjh0a1y3wrtsv0hqkrpd0mlwd0jl2m&ep=v1_gifs_related&rid=200w.gif&ct=s'
+    },
+    {
+        nombre: 'Soldado Barbaro',
+        imagen:'https://media0.giphy.com/media/ii7e1JgSrDuankMM6F/200w.gif?cid=6c09b952iji2pal69pamckj7pdmwyeq187m45k630liu5y1q&ep=v1_stickers_search&rid=200w.gif&ct=s'
+    },
+
+]
--- school-figther-2/src/views/VistaEscenarios.vue
+++ school-figther-2/src/views/VistaEscenarios.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-        <h1>Vista Escenarios</h1>
-    </div>
-</template> 
-
-<script>
-export default{
-    name: 'VistaEscenarios'
-}
-</script>
+    <section class="h-screen  bg-center  flex justify-center flex-col items-center landscape:bg-[url('https://cdn.vectorstock.com/i/500p/23/96/ring-octagon-arena-for-boxing-fight-and-mma-vector-45072396.jpg')]">
+     <h1 class="text-5xl font-bold text-center text-white p-5 ">SCHOOL FIGTHER</h1>
+     <p  class="w-[60%] text-1xl  text-center text-white mb-5 ">SELECCIONA EL ESCENARIO QUE MAS TE GUSTE</p>
+     <div class="flex w-full justify-center items-center gap-6" >
+         
+         <select class="bg-white border border-gray-300 text-start rounded-lg focus:ring-blue-50 block w-1/3 scroll-pl-2.5" v-model="personaje1">
+             <option v-for="(personaje, index) in personajes " :key="index" :value="index">{{ personaje.nombre }}</option>
+         </select>
+         {{ personaje1 }}
+ 
+     </div>
+     <div class="flex justify-center my-5">
+         <button class="bg-[#3DBC1E] text-white px-4 py-2 rounded font-bold">Continuar</button>
+     </div>
+    </section>
+ </template> 
+ 
+ <script>
+ import {personajes} from '../db/db.js'
+ export default{
+     name: 'VistaEscenarios',
+     data(){
+         return{
+             personajes: personajes,
+             personaje1: '',
+             personaje2: ''
+         }
+     }
+ }
+ </script>
